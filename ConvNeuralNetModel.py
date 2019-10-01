943ac9562a0b8a02a59b348f55e35344dd655a07
Savya Saachi <savyasaachi.verma@gmail.com>
2019-10-01 13:28:49 -0500

    Added comments and SUmmary pdf file

--- ConvNeuralNetModel.py
+++ ConvNeuralNetModel.py
@@ -3,40 +3,48 @@ import torch.nn as nn
 
 
 class ConvNeuralNet(nn.Module):
-    """ The convolution neural network model.
+    """ The deep convolution neural network model.
         All the models have to inherit the nn.Module."""
 
     def __init__(self):
         super(ConvNeuralNet, self).__init__()
 
         self.layers = nn.Sequential(
+            # Convolution Layer - 1
             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=4, stride=1,
                       padding=2),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
+            # Convolution Layer - 2
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
                       padding=2),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(p=0.05),
+            # Convolution Layer - 3
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
                       padding=2),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
+            # Convolution Layer - 4
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
                       padding=2),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(p=0.05),
+            # Convolution Layer - 5
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
                       padding=2),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
+            # Convolution Layer - 6
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1,
                       padding=0),
             nn.Dropout(p=0.05),
+            # Convolution Layer - 7
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1,
                       padding=0),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
+            # Convolution Layer - 8
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1,
                       padding=0),
             nn.BatchNorm2d(64),
@@ -55,6 +63,7 @@ class ConvNeuralNet(nn.Module):
 
     def forward(self, x):
         out = self.layers(x)
+        """Linear layer requires the tensor to be flattened"""
         out = out.reshape(out.size(0), -1)
         #print(out.shape)
         out = self.fully_connected(out)
--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import os
+
 import numpy as np
 import random
+
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+
 from torch import optim
 from torch.autograd import Variable
 from torch.backends import cudnn
 from torch.utils import data
 from ConvNeuralNetModel import ConvNeuralNet
 
+"""Directory where the the data sets will be stored"""
 data_dir = "./Data"
+
+"""HYPER-PARAMETERS"""
+
+"""number of input samples in one batch for training and testing"""
 batch_size_train = 64
 batch_size_test = 32
+"""learning rate for optimization algorithm (USED ADAM)"""
 learning_rate = 0.001
+"""number of times to traverse the entire training dataset"""
 epochs = 1
+"""bool: specifies if the model's state should be loaded from the checkpoint file"""
 load_chkpt = False
 
 
 def main():
-    """Transformations for Augmenting Training and Test Dataset"""
+
+    """Transformations for Augmenting + Conversion to tensors fot PIL Images in Training and Test Dataset
+    Referenced: Stanford CS231n: Convolutional Neural Networks for Visual Recognition, Lecture notes, "Convolutional
+    Neural Networks (CNNs / ConvNets)"""
+
     augment_train_ds = transforms.Compose([
         transforms.RandomCrop(32, padding=2),
         transforms.RandomHorizontalFlip(),
@@ -33,20 +48,23 @@ def main():
         transforms.Normalize((0.5, 0.5, 0.5), (0.2, 0.2, 0.2)),
     ])
 
-    """Set seed."""
+    """Setting seed values manually"""
     torch.manual_seed(0)
     torch.cuda.manual_seed(0)
     np.random.seed(0)
     random.seed(0)
 
-    """Loading the datasets from torchvision dataset library"""
+    """Downloading the data sets by applying the above transformations from torchvision dataset library"""
     train_ds = torchvision.datasets.CIFAR10(root=data_dir, train=True, download=True,
                                             transform=augment_train_ds)
     test_ds = torchvision.datasets.CIFAR10(root=data_dir, train=False, download=True,
                                            transform=augment_test_ds)
+
+    """Using the Dataloader to divide the data into batches and performing shuffling """
     train_ds_loader = data.DataLoader(train_ds, batch_size=batch_size_train, shuffle=True, num_workers=4)
     test_ds_loader = data.DataLoader(test_ds, batch_size=batch_size_test, shuffle=False, num_workers=4)
 
+    """Setting device to CUDA if it is available"""
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     print("Initializing Model")
@@ -55,19 +73,23 @@ def main():
     start_epoch = 0
 
     if load_chkpt:
-        print("Loading Saved Model")
+        print("Saved Model is being loaded")
         chkpt = torch.load('./Checkpoint/model_state.pt')
         conv_net.load_state_dict(chkpt['conv_net_model'])
         start_epoch = chkpt['epoch']
 
+    """If multiple GPUs are available then use asynchronous training """
     if device == 'cuda':
         conv_net = torch.nn.DataParallel(conv_net)
         cudnn.benchmark = True
 
-    """Training"""
+    """___________ Training ___________"""
+
     print("Starting Training")
 
+    """Criterion Function: Softmax + Log-Likelihood"""
     loss_fn = nn.CrossEntropyLoss()
+    """Adam Optimizer (as it takes advantage of both RMSDrop and Momentum"""
     optimizer = optim.Adam(conv_net.parameters(), lr=learning_rate)
 
     for epoch in range(start_epoch, epochs):
@@ -78,18 +100,22 @@ def main():
 
         for i, (inputs, labels) in enumerate(train_ds_loader):
 
+            """Transfer inputs and labels to CUDA if available"""
             inputs = inputs.to(device)
             labels = labels.to(device)
 
+            """Loss function requires the inputs to be wrapped in variables"""
             inputs = Variable(inputs)
 
+            """Torch tends to take cumulative gradients which is not required so setting it to zero after each batch"""
             optimizer.zero_grad()
 
             outputs = conv_net(inputs)
             loss = loss_fn(outputs, labels)
             loss.backward()
 
-            if epoch > 16:
+            """ Overflow error in the optimizer if the step size is not reset."""
+            if epoch > 8:
                 for group in optimizer.param_groups:
                     for p in group['params']:
                         state = optimizer.state[p]
@@ -105,13 +131,16 @@ def main():
             total_samples += labels.shape[0]
             # arr = (predicted_label == labels).numpy()
             # print(np.sum(arr))
+            """can not use numpy as the tensors are in CUDA"""
             total_correct += predicted_label.eq(labels.long()).float().sum().item()
             accuracy = total_correct / total_samples
+
             if i % 100 == 0:
                 print('Training [epoch: %d, batch: %d] loss: %.3f, accuracy: %.5f' %
                       (epoch + 1, i + 1, cur_loss, accuracy))
 
-        if epoch % 5 == 0:
+        """Saving model after every 5 epochs"""
+        if (epoch + 1) % 5 == 0:
             print('==> Saving model ...')
             state = {
                 'conv_net_model': conv_net.state_dict(),
@@ -121,16 +150,17 @@ def main():
                 os.mkdir('Checkpoint')
             torch.save(state, './Checkpoint/model_state.pt')
 
-    print("Training Completed")
+    print("Training Completed!")
 
-    """Testing"""
+    """___________ Testing ____________"""
     print("Testing Started")
+    """Puts model in testing state"""
     conv_net.eval()
 
     cur_loss = 0
     total_correct = 0
     total_samples = 0
-
+    """Do testing under the no_grad() context so that torch does not store/use these actions to calculate gradients"""
     with torch.no_grad():
         for i, (inputs, labels) in enumerate(test_ds_loader):
             inputs = inputs.to(device)
@@ -149,9 +179,11 @@ def main():
             # arr = (predicted_label == labels).numpy()
             total_correct += predicted_label.eq(labels.long()).float().sum().item()
             accuracy = total_correct / total_samples
+
             if i % 50 == 0:
                 print('Testing [batch: %d] loss: %.3f, accuracy: %.5f' %
                       (i + 1, cur_loss, accuracy))
+
     print("Testing Completed with accuracy:" + str(accuracy))
 
 
